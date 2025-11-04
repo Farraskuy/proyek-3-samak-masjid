0cd210eb695d7e6aba5c11791c6d6d5aef36092c
furis <yukannahinoishi@gmail.com>
2025-11-04 15:49:06 +0700

    ??

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,2 +1,4 @@
-
-<p><img src="/storage/news/images/6909a93e89463_abstract.jpg"></p>
+<p>
+  <img src="/storage/news/images/6909a93e89463_abstract.jpg"
+       style="width:100%; height:auto; object-fit:cover;">
+</p>
