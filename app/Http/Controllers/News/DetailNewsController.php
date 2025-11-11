a7a3a331d33ed787a48f4e99cd1d9c599574a36f
furis <yukannahinoishi@gmail.com>
2025-11-11 09:26:43 +0700

    checkpoint hari ini dengan catatan ada bug di dalam add berita

--- app/Http/Controllers/News/DetailNewsController.php
+++ app/Http/Controllers/News/DetailNewsController.php
@@ -9,6 +9,29 @@ class DetailNewsController extends Controller
 {
     function return_resource($slug_from_view){
         $data_posts= \DB::table('posts')->select('content')->where('slug',$slug_from_view)->first();
-        return view('post.fitur_detail_postingan',['data_posts'=> $data_posts]);
+
+    
+        $kontent_html_tag = $data_posts->content;
+
+        $obj_html = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $obj_html->loadHTML($kontent_html_tag, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+
+        $img = $obj_html->getElementsByTagName("img");
+        
+        
+        foreach($img as $image_tag){
+            $src = $image_tag->getAttribute('src');  
+                   if (!str_starts_with($src, '/storage/')) {
+            $image_tag->setAttribute('src', '/storage/' . $src);
+        }
+        }
+
+           $updated_html = $obj_html->saveHTML();
+
+           return view('post.fitur_detail_postingan',['data_posts' => $updated_html]);
+
     }
 }
+
--- resources/views/post/fitur_detail_postingan.blade.php
+++ resources/views/post/fitur_detail_postingan.blade.php
@@ -3,6 +3,8 @@
 </head>
 <body>
 <div class="ql-editor">
- {!!    $data_posts->content !!}
+ {!!    $data_posts !!}
+<!-- <img src="/storage/news/quill/6911429c8afbb.png"> -->
+<!-- /storage/ -->
 </div>
 </body>
