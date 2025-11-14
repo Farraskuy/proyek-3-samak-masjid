2dad44bb24330ef465a7bb976a5129d283808b78
furis <yukannahinoishi@gmail.com>
2025-11-15 01:31:42 +0700

    checkpoint

--- resources/views/post/halaman_postingan.blade.php
+++ resources/views/post/halaman_postingan.blade.php
@@ -109,7 +109,48 @@
             box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
         }
 
-        
+
+
+
+
+
+/* untuk style filter */
+.filter-container {
+    
+    float: right; 
+    
+    display: flex;
+    align-items: center; 
+    margin-bottom: 20px; 
+}
+
+.filter-container label {
+    margin-right: 10px; 
+    font-size: 1.1em;
+    font-weight: bold;
+    color: #333;
+}
+
+.filter-container select {
+ 
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+
+
+    font-size: 1em;
+    padding: 8px 12px;
+    border: none;
+    border-bottom: 2px solid #333;
+    background-color: transparent; 
+    cursor: pointer;
+
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23333333%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-13%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2013l128%20128c3.5%203.5%207.8%205.4%2013%205.4s9.5-1.8%2013-5.4l128-128c3.5-3.5%205.4-7.8%205.4-13%200-5-1.8-9.3-5.4-13z%22%2F%3E%3C%2Fsvg%3E');
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+    background-size: 12px;
+    padding-right: 35px; 
+}
 
 
 
@@ -136,59 +177,20 @@
 
 
         
-        <div class="filter-container">
+    <form action="/posts" method="get"></form>
+        <div class="filter-container" >
             <label for="filter-select">filter</label>
-            <select name="filter" id="filter-select">
+            <select name="filter" id="filter-select" onchange="this.form.submit()">
                 <option value="terbaru">Terbaru</option>
                 <option value="terlama">Terlama</option>
                 <option value="populer">Paling Populer</option>
             </select>
         </div>
+    </form>
 
 
 
 
-
-
-.filter-container {
-    /* Menggunakan float untuk memposisikan ke kanan */
-    float: right; 
-    
-    /* Menggunakan flexbox untuk merapikan label dan select */
-    display: flex;
-    align-items: center; /* Menjaga agar sejajar secara vertikal */
-    margin-bottom: 20px; /* Memberi jarak dari konten di bawahnya */
-}
-
-.filter-container label {
-    margin-right: 10px; /* Jarak antara teks "filter" dan box-nya */
-    font-size: 1.1em;
-    font-weight: bold;
-    color: #333;
-}
-
-.filter-container select {
-    /* Menghilangkan tampilan default browser */
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-
-    /* Styling kustom */
-    font-size: 1em;
-    padding: 8px 12px;
-    border: none; /* Menghilangkan border standar */
-    border-bottom: 2px solid #333; /* Border bawah seperti di wireframe */
-    background-color: transparent; /* Transparan agar menyatu */
-    cursor: pointer;
-
-    /* Menambahkan panah dropdown kustom */
-    /* Ini adalah SVG inline untuk panah ke bawah */
-    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23333333%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-13%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2013l128%20128c3.5%203.5%207.8%205.4%2013%205.4s9.5-1.8%2013-5.4l128-128c3.5-3.5%205.4-7.8%205.4-13%200-5-1.8-9.3-5.4-13z%22%2F%3E%3C%2Fsvg%3E');
-    background-repeat: no-repeat;
-    background-position: right 10px center;
-    background-size: 12px;
-    padding-right: 35px; /* Memberi ruang untuk panah */
-}
 
 
 
