fc8e287a49938b55f844d0b3ca3a49c5a8b35b76
furis <yukannahinoishi@gmail.com>
2025-11-14 12:47:09 +0700

    menambahkan pagination tapi untuk fitur filter belum fix tetapi untuk pagination sudah fix

--- app/Http/Controllers/News/HalamanPostinganController.php
+++ app/Http/Controllers/News/HalamanPostinganController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HalamanPostinganController extends Controller
 {
         function return_resource(){
-        $data_posts= \DB::table('posts')->select('*')->get();
+        $data_posts= \DB::table('posts')->select('*')->paginate(9);
         return view('post.halaman_postingan',['data_posts'=> $data_posts]);
     }
 }
