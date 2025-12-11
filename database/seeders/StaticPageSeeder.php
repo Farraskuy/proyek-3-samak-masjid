861ad283819c0b74b6eb0d24b3ef4bde7541a18e
Farras Fadhil S. <farrasfadhil06@gmail.com>
2025-12-11 10:22:32 +0700

    fix: memperbaiki seeder Role dan StaticPage

--- database/seeders/StaticPageSeeder.php
+++ database/seeders/StaticPageSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\StaticPage;
 use Illuminate\Database\Seeder;
 
 class StaticPageSeeder extends Seeder
@@ -13,7 +12,7 @@ class StaticPageSeeder extends Seeder
     public function run(): void
     {
         // Create default "Tentang Kami" page if it doesn't exist
-        StaticPage::firstOrCreate(
+        \App\Models\WebsiteInformation::firstOrCreate(
             ['slug' => 'tentang-kami'],
             [
                 'title' => 'Tentang Kami',
@@ -23,14 +22,20 @@ class StaticPageSeeder extends Seeder
 <h3>Visi Kami</h3>
 <p>Membangun komunitas muslim yang kuat, bersatu, dan berdampak positif bagi lingkungan sekitar.</p>
 <h3>Misi Kami</h3>
+<p>
 <ul>
 <li>Menyelenggarakan kegiatan ibadah dan dakwah yang berkualitas</li>
 <li>Memberikan layanan pendidikan agama yang komprehensif</li>
 <li>Memfasilitasi program-program sosial untuk masyarakat</li>
 <li>Membangun solidaritas dan persatuan dalam komunitas</li>
 </ul>
+</p>
 <p>Bergabunglah dengan kami dalam perjalanan ini untuk menciptakan perubahan positif.</p>',
-                'featured_image_url' => "static-pages/images/692f67f05d65a_masjid.jpg",
+                'featured_image_url' => "website-information/images/default_masjid.jpg",
+                'footer_address' => 'Jl. Masjid No. 1, Kota Samak',
+                'footer_phone' => '08123456789',
+                'footer_email' => 'info@samakmasjid.com',
+                'footer_social_links' => json_encode(['facebook' => 'https://facebook.com', 'instagram' => 'https://instagram.com']),
                 'updated_by_admin' => 1
             ]
         );
