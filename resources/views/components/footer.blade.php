d3694b37fc4a425709ce121a1f38f80055833f60
furis <yukannahinoishi@gmail.com>
2025-12-12 09:41:02 +0700

    memperbaiki footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,98 +1,145 @@
 @php
     $footerInfo = \App\Models\WebsiteInformation::first();
-    $socials = $footerInfo ? $footerInfo->footer_social_links : [];
+    $rawSocials = $footerInfo ? $footerInfo->footer_social_links : [];
+    $socials = [];
+
+    // Logika pembersih data (Sama seperti sebelumnya biar aman)
+    if (!empty($rawSocials)) {
+        // Cek jika data masih string JSON, decode dulu
+        if (is_string($rawSocials)) {
+            $rawSocials = json_decode($rawSocials, true);
+        }
+
+        if (is_array($rawSocials)) {
+            foreach ($rawSocials as $item) {
+                $platform = is_array($item) ? ($item['platform'] ?? '') : ($item->platform ?? '');
+                $url      = is_array($item) ? ($item['url'] ?? '')      : ($item->url ?? '');
+
+                if (!empty($platform) && !empty($url)) {
+                    $socials[] = [
+                        'platform' => strtolower($platform),
+                        'url'      => $url
+                    ];
+                }
+            }
+        }
+    }
 @endphp
+
 <footer class="text-white pt-5" style="background-color: #175C9E">
     <div class="container">
         <div class="row">
 
+            {{-- Kolom 1 --}}
             <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                <h6 class="text-uppercase fw-bold mb-4">
-                    <i class="fas fa-mosque me-2 text-warning"></i>SAMAK-Masjid
+                <h6 class="text-uppercase fw-bold mb-4 d-flex align-items-center">
+                    {{-- Icon Masjid (SVG) --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-building me-2 text-warning" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                        <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                    </svg>
+                    SAMAK-Masjid
                 </h6>
                 <p class="text-white-50">
-                    Sistem Administrasi Masjid Masjid yang melayani sivitas akademika dengan berbagai program kegiatan
-                    islami.
+                    Sistem Administrasi Masjid yang melayani sivitas akademika dengan berbagai program kegiatan islami.
                 </p>
             </div>
 
+            {{-- Kolom 2: Quick Links --}}
             <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
                 <h6 class="text-uppercase fw-bold mb-4 text-warning">Quick Links</h6>
                 <ul class="list-unstyled mb-0">
-                    <li class="mb-2">
-                        <a href="/" class="footer-link">Beranda</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="/jadwal-kegiatan" class="footer-link">Jadwal Kegiatan</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="/donasi" class="footer-link">Donasi</a>
-                    </li>
-                    <li class="mb-2">
-                        <a href="/laporan-keuangan" class="footer-link">Transparansi Keuangan</a>
-                    </li>
+                    <li class="mb-2"><a href="/" class="footer-link text-white text-decoration-none">Beranda</a></li>
+                    <li class="mb-2"><a href="/jadwal-kegiatan" class="footer-link text-white text-decoration-none">Jadwal Kegiatan</a></li>
+                    <li class="mb-2"><a href="/donasi" class="footer-link text-white text-decoration-none">Donasi</a></li>
+                    <li class="mb-2"><a href="/laporan-keuangan" class="footer-link text-white text-decoration-none">Transparansi Keuangan</a></li>
                 </ul>
             </div>
 
+            {{-- Kolom 3: Kontak --}}
             <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                 <h6 class="text-uppercase fw-bold mb-4 text-warning">Kontak</h6>
                 <ul class="list-unstyled text-white-50">
                     <li class="mb-2 d-flex">
-                        <i class="fas fa-map-marker-alt me-2 pt-1"></i>
+                        {{-- Icon Map (SVG) --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill me-2 mt-1" viewBox="0 0 16 16">
+                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                        </svg>
                         <span>{{ $footerInfo->footer_address ?? 'Jl. Masjid Raya No. 123, Kota' }}</span>
                     </li>
                     <li class="mb-2 d-flex">
-                        <i class="fas fa-phone me-2 pt-1"></i>
+                        {{-- Icon Phone (SVG) --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill me-2 mt-1" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
                         <span>{{ $footerInfo->footer_phone ?? '+62 812-3456-7890' }}</span>
                     </li>
                     <li class="mb-2 d-flex">
-                        <i class="fas fa-envelope me-2 pt-1"></i>
+                        {{-- Icon Email (SVG) --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill me-2 mt-1" viewBox="0 0 16 16">
+                            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                        </svg>
                         <span>{{ $footerInfo->footer_email ?? 'info@samak-Masjid.ac.id' }}</span>
                     </li>
                 </ul>
             </div>
 
+            {{-- Kolom 4: Social Media (SVG Manual) --}}
             <div class="col-lg-3 col-md-6">
                 <h6 class="text-uppercase fw-bold mb-4 text-warning">Ikuti Kami</h6>
                 <p class="text-white-50">Dapatkan update terbaru dari kami.</p>
                 <div class="d-flex gap-2">
-                    @php
-                        // Normalize socials to array of objects
-                        if (is_array($socials) && !array_is_list($socials) && !empty($socials)) {
-                            $newSocials = [];
-                            foreach ($socials as $key => $val) {
-                                if ($val) {
-                                    $newSocials[] = ['platform' => $key, 'url' => $val];
-                                }
-                            }
-                            $socials = $newSocials;
-                        }
-                    @endphp
+                    
+                    @foreach ($socials as $social)
+                        <a href="{{ $social['url'] }}" class="text-white text-decoration-none me-3" target="_blank"
+                           title="{{ ucfirst($social['platform']) }}">
+                            
+                            {{-- FACEBOOK SVG --}}
+                            @if ($social['platform'] == 'facebook')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                </svg>
+                            
+                            {{-- INSTAGRAM SVG --}}
+                            @elseif($social['platform'] == 'instagram')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.232-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.232 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                                </svg>
+                            
+                            {{-- TWITTER/X SVG --}}
+                            @elseif($social['platform'] == 'twitter' || $social['platform'] == 'x')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
+                                </svg>
 
-                    @if (!empty($socials) && is_array($socials))
-                        @foreach ($socials as $social)
-                            @if (!empty($social['url']))
-                                <a href="{{ $social['url'] }}" class="social-icon" target="_blank"
-                                    title="{{ ucfirst($social['platform'] ?? 'link') }}">
-                                    @if (($social['platform'] ?? '') == 'facebook')
-                                        <i class="fab fa-facebook-f"></i>
-                                    @elseif(($social['platform'] ?? '') == 'instagram')
-                                        <i class="fab fa-instagram"></i>
-                                    @elseif(($social['platform'] ?? '') == 'twitter')
-                                        <i class="fab fa-twitter"></i>
-                                    @elseif(($social['platform'] ?? '') == 'youtube')
-                                        <i class="fab fa-youtube"></i>
-                                    @elseif(($social['platform'] ?? '') == 'tiktok')
-                                        <i class="fab fa-tiktok"></i>
-                                    @elseif(($social['platform'] ?? '') == 'linkedin')
-                                        <i class="fab fa-linkedin-in"></i>
-                                    @else
-                                        <i class="fas fa-link"></i>
-                                    @endif
-                                </a>
+                            {{-- YOUTUBE SVG --}}
+                            @elseif($social['platform'] == 'youtube')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                                </svg>
+                            
+                            {{-- TIKTOK SVG --}}
+                            @elseif($social['platform'] == 'tiktok')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"/>
+                                </svg>
+
+                            {{-- LINKEDIN SVG --}}
+                            @elseif($social['platform'] == 'linkedin')
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                                </svg>
+                            
+                            {{-- FALLBACK LINK SVG --}}
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
+                                    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
+                                </svg>
                             @endif
-                        @endforeach
-                    @endif
+                        </a>
+                    @endforeach
+
                 </div>
             </div>
 
